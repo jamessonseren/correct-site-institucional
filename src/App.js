fe1f1df2dd6d8cf0025145be0647947158e9d021
Rai Soares <ti04@starkcard.com.br>
2020-03-09 15:16:41 -0300

    Animação route

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter, Route,  } from "react-router-dom";
 import "./app.css"
 import NavBar from './components/navbar/Navbar'
 import { AnimatedSwitch } from 'react-router-transition';
+
 // Routes
 import SobreNos from './components/sobre_nos/SobreNos'
+import Contato from './components/contato/Contato'
 
 function App() {
 
   return (
     <BrowserRouter>
       <NavBar/>
-          <AnimatedSwitch atEnter={{ opacity: 0 }} atLeave={{ opacity: 0 }} atActive={{ opacity: 1 }} className="switch-wrapper">
+          <AnimatedSwitch atEnter={{ left: -1000 }} atLeave={{ left: -1000 }} atActive={{ left: 0 }} className="switch-wrapper">
             <Route path='/sobre_nos'>
               <SobreNos/>
+            </Route>
+            <Route path='/contato'>
+              <Contato/>
             </Route>
           </AnimatedSwitch>
     </BrowserRouter>
---
+++ src/components/contato/Contato.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './contato.css'
+
+export default function Contato(){
+    return(
+        <div className='contato'>contato</div>
+    )
+}
