69b37854c11cb2ea3ff20b182fe87fb73ce5f5b5
Raí Soares <ti04@starkcard.com.br>
2020-03-18 17:46:00 -0300

    Refatoração sobre nós

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route,  } from "react-router-dom";
+import { BrowserRouter, Route, Switch  } from "react-router-dom";
 import "./App.css"
 import NavBar from './components/navbar/Navbar'
-import { AnimatedSwitch } from 'react-router-transition';
 
 // Routes
 import SobreNos from './components/sobre_nos/SobreNos'
@@ -15,23 +14,23 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar/>
-          <AnimatedSwitch atEnter={{ left: -1000 }} atLeave={{ left: -2000 }} atActive={{ left: 0 }} className="switch-wrapper">
-            <Route path='/sobre_nos'>
-              <SobreNos/>
-            </Route>
-            <Route path='/produtos'>
-              <Produtos/>
-            </Route>
-            <Route path='/contato'>
-              <Contato/>
-            </Route>
-            <Route path='/vantagens'>
-              <Vantagens/>
-            </Route>
-            <Route path='/aplicativos'>
-              <Aplicativos/>
-            </Route>
-          </AnimatedSwitch>
+      <Switch>
+        <Route path='/sobre_nos'>
+          <SobreNos/>
+        </Route>
+        <Route path='/produtos'>
+          <Produtos/>
+        </Route>
+        <Route path='/contato'>
+          <Contato/>
+        </Route>
+        <Route path='/vantagens'>
+          <Vantagens/>
+        </Route>
+        <Route path='/aplicativos'>
+          <Aplicativos/>
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/components/sobre_nos/SobreNos.js
+++ src/components/sobre_nos/SobreNos.js
@@ -1,22 +1,54 @@
-import React from 'react';
-import {Col, Row, Container, Image} from 'react-bootstrap'
-import "./sobreNos.css"
+import React, { useState, useEffect } from 'react';
+import {Container} from 'react-bootstrap'
+import posed from 'react-pose';
+import "./SobreNos.css"
+
+const Box = posed.div({
+    visible: { 
+        x: 0,
+        opacity: 1,
+        transition: { duration: 1000 }
+    },
+    hidden: { 
+        x: -500,
+        opacity: 0.3,
+        transition: { duration: 1000 }
+    }
+});
 
 export default function SobreNos(){
+    const [isVisible, setVisible] = useState(false)
+    
+    useEffect(() => {
+        setVisible(true)
+    })
+
     return(
-    <div>
-    <Container fluid={true} className='fundoImg'>
-        
-    <h2 className="center item animated fadeInLeft">Sobre Nós<div class="fadingEffect"></div></h2>
-    </Container>
-    <Container fluid={true} className="fundo">
-        <Row className="">
-            <p className="text item animatedOne fadeInLeft">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A Tradecard surgiu no Brasil em 2018, com sede em Campo Grande/MS, com a finalidade de trazer o que existe de mais moderno e inovador, nas áreas de Recursos Humanos (benefícios), Compras, Pagamentos, Empréstimos, Economia, Saúde, Bem Estar, Diversão, Segurança e muitas outras. Fez grandes investimentos em “Tecnologia misturado com apreço aos seus Usuários”, focado nas necessidades cotidiana de seus Clientes, pessoas Físicas e Jurídicas, permitindo a eles, como novo conceito de mercado – “Liberdade de Ação e Conveniência”.</p>
-        </Row>
-        <Row className="">
-            <p className="text item animatedTwo fadeInLeft">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Para o mundo corporativo, em abril de 2020, a Tradecard lança no mercado brasileiro, grandes novidades na área de Recursos Humanos - Benefícios,  transformando o que caracteriza produtos de benefícios, como Vales Alimentação, Refeição, Combustível, Adiantamento Salarial, Empréstimos Consignado (juros reduzidos), Programas de Saúde e outros, em uma Plataforma de Auto-gestão de Benefícios, permitindo aos gestores a liberdade de acesso decisório quanto à escolha do tipo e prazo de duração, permitindo ainda a definição dos parâmetros necessários a concessão de cada benefícios, adaptando-os às suas políticas internas, leis, sindicatos e outros motivos se houverem.</p>
-        </Row>
-    </Container>
-    </div>
+        <Container fluid={true} className='p-0 m-0'>
+            <Container fluid={true} className='backgroundImage d-flex'>
+                <Box pose={isVisible ? 'visible' : 'hidden'} className='align-items-end ml-4 row'>
+                    <h1> Sobre Nós </h1>
+                </Box>
+            </Container>
+            <Container fluid={true} className="section p-5">
+                <Box pose={isVisible ? 'visible' : 'hidden'}>
+                    <p className="description ">
+                        A Tradecard surgiu no Brasil em 2018, com sede em Campo Grande/MS, 
+                        com a finalidade de trazer o que existe de mais moderno e inovador, nas áreas de Recursos Humanos (benefícios), 
+                        Compras, Pagamentos, Empréstimos, Economia, Saúde, Bem Estar, Diversão, Segurança e muitas outras. 
+                        Fez grandes investimentos em “Tecnologia misturado com apreço aos seus Usuários”, focado nas necessidades cotidiana de seus Clientes, 
+                        pessoas Físicas e Jurídicas, permitindo a eles, como novo conceito de mercado – “Liberdade de Ação e Conveniência”.
+                    </p>
+                    <p className="description">
+                        Para o mundo corporativo, em abril de 2020, a Tradecard lança no mercado brasileiro, grandes novidades na área de Recursos Humanos - 
+                        Benefícios,  transformando o que caracteriza produtos de benefícios, como Vales Alimentação, Refeição, Combustível, Adiantamento Salarial, 
+                        Empréstimos Consignado (juros reduzidos), Programas de Saúde e outros, em uma Plataforma de Auto-gestão de Benefícios, 
+                        permitindo aos gestores a liberdade de acesso decisório quanto à escolha do tipo e prazo de duração, 
+                        permitindo ainda a definição dos parâmetros necessários a concessão de cada benefícios, 
+                        adaptando-os às suas políticas internas, leis, sindicatos e outros motivos se houverem.
+                    </p>
+                </Box>
+            </Container> 
+        </Container>
     )
 }
