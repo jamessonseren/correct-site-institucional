7be740e75f44575d71ce4e768de18a21824970bf
Raí Soares <ti04@starkcard.com.br>
2020-03-27 09:22:03 -0300

    Correção sobre nós

--- src/components/sobre_nos/SobreNos.js
+++ src/components/sobre_nos/SobreNos.js
@@ -28,30 +28,14 @@ export default function SobreNos(){
 
     return(
         <Container fluid={true} className='p-0 m-0'  style={{overflow: 'hidden'}}>
-            <Row className='backgroundImage d-flex'>
-                <Box pose={isVisible ? 'visible' : 'hidden'} className='align-items-end ml-4 row'>
-                    <h1 className='font-weight-bold'> Sobre Nós </h1>
+            <Row className='backgroundImage d-flex justify-content-center'>
+                <Box pose={isVisible ? 'visible' : 'hidden'} className='align-items-center ml-5 row col-8'>
+                    <p className="description">
+                        A <span className='font-weight-bold'>Tradecard</span> surgiu no Brasil em 2018, com a finalidade de trazer o que existe de mais moderno e inovador, nas áreas de Recursos Humanos (benefícios), 
+                        <span className='font-weight-bold'> Compras, Pagamentos, Empréstimos, Economia, Saúde, Bem Estar, Diversão, Segurança e muitas outras</span>.
+                    </p>
                 </Box>
             </Row>
-            <Row fluid={true} className="section p-5">
-                <Box pose={isVisible ? 'visible' : 'hidden'}>
-                    <p className="description ">
-                        A Tradecard surgiu no Brasil em 2018, com sede em Campo Grande/MS, 
-                        com a finalidade de trazer o que existe de mais moderno e inovador, nas áreas de Recursos Humanos (benefícios), 
-                        Compras, Pagamentos, Empréstimos, Economia, Saúde, Bem Estar, Diversão, Segurança e muitas outras. 
-                        Fez grandes investimentos em “Tecnologia misturado com apreço aos seus Usuários”, focado nas necessidades cotidiana de seus Clientes, 
-                        pessoas Físicas e Jurídicas, permitindo a eles, como novo conceito de mercado – “Liberdade de Ação e Conveniência”.
-                    </p>
-                    <p className="description">
-                        Para o mundo corporativo, em abril de 2020, a Tradecard lança no mercado brasileiro, grandes novidades na área de Recursos Humanos - 
-                        Benefícios,  transformando o que caracteriza produtos de benefícios, como Vales Alimentação, Refeição, Combustível, Adiantamento Salarial, 
-                        Empréstimos Consignado (juros reduzidos), Programas de Saúde e outros, em uma Plataforma de Auto-gestão de Benefícios, 
-                        permitindo aos gestores a liberdade de acesso decisório quanto à escolha do tipo e prazo de duração, 
-                        permitindo ainda a definição dos parâmetros necessários a concessão de cada benefícios, 
-                        adaptando-os às suas políticas internas, leis, sindicatos e outros motivos se houverem.
-                    </p>
-                </Box>
-            </Row> 
         </Container>
     )
 }
