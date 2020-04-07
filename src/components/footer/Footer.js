1c20fa78d12b79b489d17540ea195a0a4d99b03c
StarkCardTi05 <ti05@starkcard.com.br>
2020-04-07 12:11:55 -0300

    Responsividade Navbar e Sobre Nos

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,35 +7,39 @@ export default function Footer() {
     return (
         <Row style={{backgroundColor: "rgba(14, 14, 14, 0.9)", padding: 20}}>
             <Col sm={2} className="justify-content-center align-items-center d-flex">
-                <Image className="p-3" style={{width: '70%'}} src={require('../../assets/robot.png')} fluid/>
+                <Image className="p-3" style={{width: '45vh'}} src={require('../../assets/robot.png')} fluid/>
             </Col>
             <Col sm={8} className="justify-content-center align-items-center d-flex" style={{flexDirection: 'column'}}>
                 <Row>
-                    <span  className='ml-2 mr-2 text-light'>
-                    <Link className='navbar-link-trade ml-2 mr-2 pl-0'>Privacidade</Link>
-                    |
-                    <Link className='navbar-link-trade ml-2 mr-2 pl-0'>Segurança</Link>
-                    |
-                    <Link className='navbar-link-trade ml-2 mr-2 pl-0' to='/contato'>Contato</Link>
-                    </span>
+                    <Col sm={12}>
+                        <span  className='ml-2 mr-2 text-light navbar-link-trade'>
+                        <Link className='navbar-link-trade pad'>Privacidade</Link>
+                        |
+                        <Link className='navbar-link-trade pad'>Segurança</Link>
+                        |
+                        <Link className='navbar-link-trade pad' to='/contato'>Contato</Link>
+                        </span>
+                    </Col>
                 </Row>
                 <Row className='mt-3'>
-                    <FaFacebook color='white' className='mr-3' size={30}/>
-                    <FaInstagram color='white' size={30}/>
+                    <FaFacebook color='white' className='mr-3 iconFooter' />
+                    <FaInstagram color='white' className=' iconFooter' />
                 </Row>
-                <div className='mt-3 text-light'>
-                    <span className='text-lightml-2 mr-2 text-light'>
-                    <Link className='text-light ml-2 mr-2'>(67) 3253-7232</Link>
-                    |
-                    <Link className='text-light ml-2 mr-2'>teste@tradecard.net.br</Link>
-                    </span>
-                </div>
+                <Row>
+                    <Col sm={12}>
+                            <span className='text-light text-light fontr'>
+                            <Link className='text-light pad'>(67) 3253-7232</Link>
+                            |
+                            <Link className='text-light pad'>teste@tradecard.net.br</Link>
+                            </span>
+                    </Col>
+                </Row>
             </Col>
             <Col sm={2} className="justify-content-center align-items-center d-flex" style={{flexDirection: 'column'}}>
-                <a> <Image src={require('../../assets/download_google_play.png')} style={{width: '50%'}} rounded fluid/> </a>
-                <a> <Image src={require('../../assets/download_app_store.png')} style={{width: '50%', marginTop: 10}} rounded fluid/> </a>
+                <a> <Image src={require('../../assets/download_google_play.png')} style={{width: '25vh', marginTop: 10}} rounded fluid/> </a>
+                <a> <Image src={require('../../assets/download_app_store.png')} style={{width: '25vh', marginTop: 10}} rounded fluid/> </a>
             </Col>
-            <Col sm={12} className="justify-content-center align-items-center d-flex" style={{color: '#ccc'}}>
+            <Col sm={12} className="justify-content-center navbar-link-trade align-items-center d-flex" style={{color: '#ccc'}}>
                 © Tradecard 2018 - 2020 - Todos os direitos reservados.
             </Col>
         </Row>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
 import { Link } from "react-router-dom";
 
 import "./navbar.css"
 
 export default function NavBar(){
     return(
-        <Nav className="justify-content-center navbar-trade">
-            <Nav.Item  className='align-items-center justify-content-center d-flex pt-0'>
-                <img src={require('../../assets/tradecard_logo_white.png')} alt='Tradecard Logo' height='40%' width='250'/>
-            </Nav.Item>
-            <Nav.Item><Link className='navbar-link-trade' to='/sobre_nos'>Sobre nós</Link></Nav.Item>
-            <Nav.Item><Link className='navbar-link-trade' to='/produtos'>Produtos</Link></Nav.Item>
-            <Nav.Item><Link className='navbar-link-trade' to='/vantagens'>Vantagens</Link></Nav.Item> 
-            <Nav.Item><Link className='navbar-link-trade' to='/aplicativos'>Aplicativos</Link></Nav.Item>
-            <Nav.Item><Link className='navbar-link-trade' to='/contato'>Contato</Link></Nav.Item>
-            <Nav.Item><Link className='navbar-link-trade' to='/trade_shop'>Tradeshop</Link></Nav.Item>
-            <Nav.Item><Link className='navbar-link-trade' to='/rede_compras'>Rede de compras</Link></Nav.Item>
-        </Nav>
+        <Navbar style={{backgroundColor: "rgba(14, 14, 14, 0.9)"}} expand="lg">
+            
+            <Navbar.Brand href="#home"  style={{ marginLeft:'17.5%'}}><img src={require('../../assets/tradecard_logo_white.png')} className="logo" alt='Tradecard Logo'/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link ><Link className='navbar-link-trade' to='/sobre_nos'>Sobre nós</Link></Nav.Link>
+                    <Nav.Link><Link className='navbar-link-trade' to='/produtos'>Produtos</Link></Nav.Link>
+                    <Nav.Link><Link className='navbar-link-trade' to='/vantagens'>Vantagens</Link></Nav.Link> 
+                    <Nav.Link><Link className='navbar-link-trade' to='/aplicativos'>Aplicativos</Link></Nav.Link>
+                    <Nav.Link><Link className='navbar-link-trade' to='/contato'>Contato</Link></Nav.Link>
+                    <Nav.Link><Link className='navbar-link-trade' to='/trade_shop'>Tradeshop</Link></Nav.Link>
+                    <Nav.Link><Link className='navbar-link-trade' to='/rede_compras'>Rede de compras</Link></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
