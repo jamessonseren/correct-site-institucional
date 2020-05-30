f5b648c2176b8c473b19e6cfc54bc70e9b1a8f64
Raí Soares <ti04@starkcard.com.br>
2020-05-30 16:05:31 -0300

    Remoção console.log

--- src/components/contato/Contato.js
+++ src/components/contato/Contato.js
@@ -53,7 +53,6 @@ class Contato extends React.Component {
 
         let response = await fetch(`https://www.correct.com.br/B5IxozrETYlXSNXj81PvDtFjVb531fVl55hNEDLK/sendEmail.php?nome=${nome}&email=${email}&message=${message}&empresa=${empresa}&tipo=${tipo}&phone=${phone}&cidade=${cidade}&apiKey=${apiKey}`, {mode: 'no-cors'})
         let result   = await response.text()
-        console.log(result)
         if(result == 1){
             this.setState({
                 err: false, 
