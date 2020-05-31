d18b1ca08edd814bcc9a3eed19ebf92d61dfa583
Raí Soares <ti04@starkcard.com.br>
2020-05-31 18:23:46 -0300

    scroll to top on page change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch  } from "react-router-dom";
 import "./App.css"
-import NavBar from './components/navbar/Navbar'
-
+import ScrollToTop from './ScrollToTop'
 // Routes
+import NavBar from './components/navbar/Navbar'
 import SobreNos from './components/sobre_nos/SobreNos'
 import Contato from './components/contato/Contato'
 import Aplicativo from './components/app/Aplicativo.js'
@@ -17,41 +17,43 @@ import Footer from './components/footer/Footer'
 function App() {
   return (
     <BrowserRouter>
-      <NavBar/> 
-      <Switch>
-        <Route exact path='/'>
-          <SobreNos/>
-          <Footer />
-        </Route>
-        <Route path='/para_voce'>
-          <Aplicativo/>
-          <Footer />
-        </Route>
-        <Route path='/para_empresas'>
-          <Empresas/>
-          <Footer />
-        </Route>
-        <Route path='/contato'>
-          <Contato/>
-          <Footer />
-        </Route>
-        <Route path='/para_seu_negocio'>
-          <Negocio/>
-          <Footer />
-        </Route>
-        <Route path='/correct_shop'>
-          <Tradeshop/>
-          <Footer />
-        </Route>
-        <Route path='/correct_club'>
-          <Clube/>
-          <Footer />
-        </Route>
-        <Route path='/rede_compras'>
-          <RedeDeCompras/>
-          <Footer/>
-        </Route>
-      </Switch>
+      <ScrollToTop>
+        <NavBar/> 
+        <Switch>
+          <Route exact path='/'>
+            <SobreNos/>
+            <Footer />
+          </Route>
+          <Route path='/para_voce'>
+            <Aplicativo/>
+            <Footer />
+          </Route>
+          <Route path='/para_empresas'>
+            <Empresas/>
+            <Footer />
+          </Route>
+          <Route path='/contato'>
+            <Contato/>
+            <Footer />
+          </Route>
+          <Route path='/para_seu_negocio'>
+            <Negocio/>
+            <Footer />
+          </Route>
+          <Route path='/correct_shop'>
+            <Tradeshop/>
+            <Footer />
+          </Route>
+          <Route path='/correct_club'>
+            <Clube/>
+            <Footer />
+          </Route>
+          <Route path='/rede_compras'>
+            <RedeDeCompras/>
+            <Footer/>
+          </Route>
+        </Switch>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
